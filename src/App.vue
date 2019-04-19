64ca4a11f14a7fc4f5bc17018ac4a2cc9ac7edfb
Emil Priver <emil@rivercode.se>
2019-04-19 16:30:47 +0200

    finished first view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+        <div class="column">
+          <a href="https://emilpriver.com">
+            <img src="https://edge.privvcdn.se/portfolio/vitlogotyp.png" />
+          </a>
+        </div>
+        <span>Global CDN network hosted by DigitalOcean</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0px;
+  padding: 0px;
+  background-image: url('https://edge.privvcdn.se/cdn_bg.jpg');
+  background-size: cover;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  color: #fff;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 50px;
+}
+
+img {
+  width: 400px;
+  height: auto;
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+  margin: 0px auto;
+}
+
+span {
+  font-size: 30px;
+}
+
+.column {
+  width: 100%;
 }
 </style>
